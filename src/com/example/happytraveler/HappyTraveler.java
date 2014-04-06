757c1216263b46e1e724ca34eedc5ae7dc342d2c
Nick Vargas <niavarga@ucsc.edu>
2014-04-06 10:27:16 -0700

    Added map to landing screen

--- src/com/example/happytraveler/HappyTraveler.java
+++ src/com/example/happytraveler/HappyTraveler.java
@@ -1,22 +1,47 @@
 package com.example.happytraveler;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.widget.Button;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class HappyTraveler extends Activity {
-	
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HappyTraveler extends FragmentActivity {
+	static final LatLng UCSANTACRUZ = new LatLng(36.98904,-122.06441);
+    static final LatLng SANTACRUZDOWNTOWN = new LatLng(36.96996,-122.02497);
+    private GoogleMap map;
 	Button driver, rider; //the two buttons
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_happy_traveler);
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker origin = map.addMarker(new MarkerOptions().position(UCSANTACRUZ)
+                .title("Santa Cruz"));
+            Marker dest = map.addMarker(new MarkerOptions()
+                .position(SANTACRUZDOWNTOWN)
+                .title("Downtown")
+                .snippet("Santa Cruz is cool")
+                .icon(BitmapDescriptorFactory
+                    .fromResource(R.drawable.ic_launcher)));
+
+        // Move the camera instantly to hamburg with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSANTACRUZ, 15));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
         addListenersOnButtons(); //Sets buttons to respond to clicks
 	}
 
